b28afd8eb1e3c69e7f0841adf2bdcebddfe22e86
avinashreddy.pyata@gmail.com <avinashreddy.pyata@gmail.com>
2018-06-06 20:32:25 -0400

    Added Comments

--- log4j2-xml-example/src/main/java/com/log4jtwo/tutorial/MainApp.java
+++ log4j2-xml-example/src/main/java/com/log4jtwo/tutorial/MainApp.java
@@ -3,10 +3,22 @@ package com.log4jtwo.tutorial;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class MainApp for Using the Log4j2 using XML Configuration File.
+ */
 public class MainApp {
+	
+	
 
+   /** The Constant logger. */
    private static final Logger logger = LogManager.getLogger(MainApp.class);
 
+   /**
+    * The main method showing the implementation of Log Files
+    *
+    * @param args the arguments
+    */
    public static void main(String[] args) {
 
       for(int i = 0; i <10000; i++){
